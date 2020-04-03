6b79b1dc4340671b4ac078214586fb0880d8ed7c
Nikita Z <nikita.z@smartym.pro>
2020-04-03 13:14:20 +0300

    show more button

---
+++ src/app/components/users-list/users-list.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-users-list',
+  templateUrl: './users-list.component.html',
+  styleUrls: ['./users-list.component.scss'],
+})
+export class UsersListComponent implements OnInit {
+  @Input() UserList;
+  displayAmount;
+  constructor() {
+    this.displayAmount = 8;
+  }
+
+  ngOnInit(): void {
+  }
+  showMoreUsers() {
+    this.displayAmount = this.displayAmount + 10;
+  }
+}
